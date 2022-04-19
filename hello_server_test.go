a186505a5ce7fe5a5212568c6842504fdcb25b7c
nofarb <nofarb@gmail.com>
2022-04-18 20:46:23 -0500

    added tests

--- hello_server_test.go
+++ hello_server_test.go
@@ -7,9 +7,67 @@ import (
 
 
 func TestGreetingSpecificJohn(t *testing.T) {
-	time.Sleep(8 * time.Second)
 	greeting := CreateGreeting("John")
 	if greeting != "Hello, John\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+func TestGreetingSpecificJohn1(t *testing.T) {
+	time.Sleep(1 * time.Second)
+	greeting := CreateGreeting("John1")
+	if greeting != "Hello, John1\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+
+func TestGreetingSpecificJohn2(t *testing.T) {
+	time.Sleep(2 * time.Second)
+	greeting := CreateGreeting("John2")
+	if greeting != "Hello, John2\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+
+func TestGreetingSpecificJohn3(t *testing.T) {
+	time.Sleep(3 * time.Second)
+	greeting := CreateGreeting("John3")
+	if greeting != "Hello, John\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+func TestGreetingSpecificJohn4(t *testing.T) {
+	time.Sleep(4 * time.Second)
+	greeting := CreateGreeting("John4")
+	if greeting != "Hello, John4\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+func TestGreetingSpecificJohn5(t *testing.T) {
+	time.Sleep(5 * time.Second)
+	greeting := CreateGreeting("John5")
+	if greeting != "Hello, John5\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+func TestGreetingSpecificJohn6(t *testing.T) {
+	time.Sleep(6 * time.Second)
+	greeting := CreateGreeting("John6")
+	if greeting != "Hello, John6\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
+	}
+}
+
+
+func TestGreetingSpecificJohn7(t *testing.T) {
+	time.Sleep(7 * time.Second)
+	greeting := CreateGreeting("John7")
+	if greeting != "Hello, John7\n" {
 		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, John\n")
 	}
 }
